90efb0c7f04b5d44d78db55c1d21760c3c6a8dc4
Mathieu <vinot.mathieu88@gmail.com>
2022-03-10 13:33:00 +0100

    ControleurCOmpte  -> Test login déjà existant                   -> Créer le compte

--- src/php/controleur/ControleurCompte.php
+++ src/php/controleur/ControleurCompte.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace custumbox\php\controleur;
 
 // IMPORTS
+use custumbox\php\Vue\VueUtilisateur;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Container;
 use custumbox\php\Modele\Compte;
 
@@ -19,12 +22,35 @@ class ControleurCompte
     }
 
     // METHODES
+
     private function creerCompteInBDD(array $args) : void{
-        $c = new Compte();
+        $c = new utilisateur();
         $c->Login = filter_var($args['Login'], FILTER_SANITIZE_STRING);
         $c->Mdp = password_hash(filter_var($args['Mdp'], FILTER_SANITIZE_STRING), PASSWORD_DEFAULT);
         $c->Mail = filter_var($args['Mail'], FILTER_SANITIZE_EMAIL);
         $c->Telephone = filter_var($args['Telephone'], FILTER_SANITIZE_STRING);
         $c->save();
+    }
+
+    private function loginValide( string $login) : bool{
+        $res =  utilisateur::where('login', '=', $login)->get();
+        $r = $res->count();
+        if ($r==0) return true;
+        else return false;
+    }
+
+    public function creerCompte(Request $rq, Response $rs, array $args): Response {
+        try {
+            $vue = new VueUtilisateur($this->container);
+
+            if (sizeof($args) == 4) {
+                if ($this->loginValide(filter_var($args['login'], FILTER_SANITIZE_STRING)) == true) {
+                    $this->creerCompteInBDD($args);
+                }
+            }
+        }catch (\Exception $e) {
+            $rs->getBody()->write("Erreur dans la creation d'un compte...<br>".$e->getMessage()."<br>".$e->getTrace());
+        }
+        return $rs;
     }
 }
