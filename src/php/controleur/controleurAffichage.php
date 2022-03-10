5abdf1c28dc22e7c3a5ad5a0a8c46b109a560fa3
Mathieu <vinot.mathieu88@gmail.com>
2022-03-10 10:38:40 +0100

    controleur affichage constructeur

--- src/php/controleur/controleurAffichage.php
+++ src/php/controleur/controleurAffichage.php
@@ -2,4 +2,22 @@
 declare(strict_types=1);
 
 // NAMESPACE
-namespace controler;
+namespace custumbox\php\controleur;
+
+// IMPORTS
+use Slim\Container;
+
+/**
+ * Classe controleurAffichage,
+ * Controleur sur l'affichage des produits
+ */
+class controleurAffichage
+{
+    // ATTRIBUTS
+    private $container;
+
+    // CONSTRUCTEUR
+    public function __construct(Container $container) {
+        $this->container = $container;
+    }
+}
